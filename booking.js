0d0419ab05fd14bd14b38b148c4db0beeb173ced
NPSfah <84497623+NPSfah@users.noreply.github.com>
2025-03-11 22:54:56 +0700

    init

---
+++ booking.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("booking-form").addEventListener("submit", function (event) {
+        let joinLandChecked = document.getElementById("join-land").checked;
+        if (joinLandChecked) {
+            let confirmChoice = confirm("Do you want to purchase your own airfare (Join Land)?");
+            if (confirmChoice) {
+                alert("You chose to purchase your own airfare (Join Land)");
+            } else {
+                event.preventDefault();
+            }
+        }
+    });
+});
